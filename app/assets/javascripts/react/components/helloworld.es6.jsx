3e56a38b94196118e28563a6bd11eb39fbe3e52c
aashkir <ahmedaashkir@gmail.com>
2019-06-16 18:27:44 -0400

    Changed the RaisedButton colour to red

--- app/assets/javascripts/react/components/helloworld.es6.jsx
+++ app/assets/javascripts/react/components/helloworld.es6.jsx
@@ -2,8 +2,8 @@ class HelloWorld extends React.Component {
   render() {
     return (
       <div>
-      <p>Hello World</p>
-      <MaterialUi.RaisedButton label="HelloWorld" />
+        <p>Hello World</p>
+        <MaterialUi.RaisedButton label="HelloWorld" backgroundColor="red"/>
       </div>
     )
   }
