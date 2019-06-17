728f027c87b087d53f12ac8f2202f3ac7ffe3e50
aashkir <ahmedaashkir@gmail.com>
2019-06-17 16:25:19 -0400

    fixed theme

--- app/assets/javascripts/react/components/helloworld.es6.jsx
+++ app/assets/javascripts/react/components/helloworld.es6.jsx
@@ -1,9 +1,10 @@
 class HelloWorld extends React.Component {
   render() {
+
     return (
       <div>
         <p>Hello World</p>
-        <MaterialUi.RaisedButton label="HelloWorld" backgroundColor="red" onClick={() => {alert('Congratulations, we made it');}}/>
+        <MaterialUi.RaisedButton label="HelloWorld" primary="Colors.red" onClick={() => {alert('Congratulations, we made it');}}/>
       </div>
     )
   }
